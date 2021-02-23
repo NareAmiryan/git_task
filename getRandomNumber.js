4333aa2fa27984959772027e186481a43c0ac7f8
NareAmiryan <67760359+NareAmiryan@users.noreply.github.com>
2021-02-23 14:24:52 +0400

    the final commit

--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,11 +1,18 @@
-function getRandomLetter(){
-    let randLetter = "c";
-    return randLetter;
-} 
-
-console.log(getRandomLetter());
-
-//I want to add a new function
-function getString (){
-    return "my new function's return part";
+function getRandomNumber(){
+    return Math.random();
 }
+
+console.log(getRandomNumber());
+
+//I want to add a new function 
+function arrRev(myArr){
+    let newArr = new Array;
+    for(let i = myArr.length - 1; i >= 0; i--){
+        newArr.push(myArr[i]);
+    }
+    return newArr;
+}
+
+let numbers = [5,6,7,14,8,18,5];
+let newArray = arrRev(numbers);
+alert("Our new array is : " + newArray);
