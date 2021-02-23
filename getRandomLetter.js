68c09ee12e7d481fb5e8936ee50adc97aa9495c3
NareAmiryan <67760359+NareAmiryan@users.noreply.github.com>
2021-02-23 14:23:45 +0400

    commmittt

--- getRandomLetter.js
+++ getRandomLetter.js
@@ -3,3 +3,16 @@ function getRandomNumber(){
 }
 
 console.log(getRandomNumber());
+
+//I want to add a new function 
+function arrRev(myArr){
+    let newArr = new Array;
+    for(let i = myArr.length - 1; i >= 0; i--){
+        newArr.push(myArr[i]);
+    }
+    return newArr;
+}
+
+let numbers = [5,6,7,14,8,18,5];
+let newArray = arrRev(numbers);
+alert("Our new array is : " + newArray);
